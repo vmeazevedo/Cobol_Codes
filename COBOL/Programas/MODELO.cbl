a8630658424590c0f6889ce88ff288f0a6b9c0ae
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-15 18:00:41 -0300

    modelo

--- COBOL/Programas/MODELO.cbl
+++ COBOL/Programas/MODELO.cbl
@@ -22,28 +22,51 @@
 
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT ARQUIVO ASSIGN TO LOCAL.
+           SELECT EMPREGADOS ASSIGN TO LOCAL
+           ORGANIZATION IS INDEXED
+           ACCESS MODE IS DYNAMIC
+           RECORD KEY IS EMPREGADOS-CHAVE
+           FILE STATUS IS WS-CODIGO.
        I-O-CONTROL.
 
       *-----------------------------------------------------------------
        DATA DIVISION.
       *-----------------------------------------------------------------
        FILE SECTION.
-       FD  ARQUIVO
-           LABEL RECORDS ARE STANDARD
+       FD  EMPREGADOS
            RECORD CONTAINS 50 CHARACTERS
-           BLOCK CONTAINS 0 RECORDS
-           DATA RECORD IS REG-ARQUIVO.
-       01  REG-ARQUIVO      PIC X(50).
+           DATA RECORD IS REG-EMPREGADOS.
+       01  REG-EMPREGADOS.
+           05 EMPREGADOS-CHAVE      PIC 9(05).
+           05 FILLER                PIC X(45).
 
 
        WORKING-STORAGE SECTION.
+       01  WS-CODIGO           PIC X(02).
+       01  WS-CHAVE            PIC X(02).
+       01  WS-REG-EMPREGADOS   PIC X(10).
 
       *-----------------------------------------------------------------
        PROCEDURE DIVISION.
       *-----------------------------------------------------------------
        PROGRAM-BEGIN.
+           020-ABRE-ARQUIVOS.
+               OPEN INPUT EMPREGADOS.
+               IF WS-CODIGO NOT EQUAL ZEROS
+                   DISPLAY 'ERROR AO ABRIR O ARQUIVO EMPREGADOS'.
+           020-FIM. EXIT.
 
+           040-LE-EMPREGADO.
+               MOVE WS-CHAVE TO EMPREGADOS-CHAVE
+               READ EMPREGADOS
+               IF WS-CODIGO NOT EQUAL ZEROS
+                   DISPLAY 'NAO EXISTE REGISTRO'.
+               MOVE REG-EMPREGADOS TO WS-REG-EMPREGADOS.
+           040-FIN. EXIT.
+
+           050-FECHA-ARQUIVOS.
+               CLOSE-EMPREGADOS.
+           050-FIM. EXIT.
 
        PROGRAM-DONE.
            GOBACK.
