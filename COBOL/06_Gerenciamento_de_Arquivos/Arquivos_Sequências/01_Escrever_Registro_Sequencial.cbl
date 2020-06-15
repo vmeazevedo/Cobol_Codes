5b643d3f2130ab738882fe920a1dee6fcfd2baec
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-15 18:01:46 -0300

    Create 01_Escrever_Registro_Sequencial.cbl

---
+++ COBOL/06_Gerenciamento_de_Arquivos/Arquivos_Sequências/01_Escrever_Registro_Sequencial.cbl
@@ -0,0 +1,60 @@
+      *-----------------------------------------------------------------
+       IDENTIFICATION DIVISION.
+      *-----------------------------------------------------------------
+       PROGRAM-ID.     01_Escrever_Registro.
+       AUTHOR.         VINICIUS AZEVEDO.
+       DATE-WRITTEN.   12/06/2020.
+       DATE-COMPILED.
+       SECURITY.       NAO E CONFIDENCIAL.
+      ******************************************************************
+      * TEXTO: Exemplificação de escrita em um arquivo.
+      *    Foi aberto um arquivo .txt e apresentado no external log.
+      *    É importante lembrar que o .txt deve estar na pasta bin
+      *do diretório do programa.
+      ******************************************************************
+      *-----------------------------------------------------------------
+       ENVIRONMENT DIVISION.
+      *-----------------------------------------------------------------
+       CONFIGURATION SECTION.
+       SOURCE-COMPUTER.           IBM-3083.
+       OBJECT-COMPUTER.           IBM-3083.
+       SPECIAL-NAMES.             DECIMAL-POINT IS COMMA.
+
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT STUDENT ASSIGN TO 'input3.dat'
+           ORGANIZATION IS LINE SEQUENTIAL
+           ACCESS IS SEQUENTIAL.
+
+      *-----------------------------------------------------------------
+       DATA DIVISION.
+      *-----------------------------------------------------------------
+       FILE SECTION.
+       FD  STUDENT.
+       01  STUDENT-FILE.
+           05  STUDENT-ID  PIC 9(5).
+           05  NAME        PIC A(25).
+
+       WORKING-STORAGE SECTION.
+       01  WS-STUDENT.
+           05  WS-STUDENT-ID       PIC 9(05).
+           05  FILLER              PIC X(01) VALUE SPACES.
+           05  WS-NAME             PIC A(25).
+
+      *-----------------------------------------------------------------
+       PROCEDURE DIVISION.
+      *-----------------------------------------------------------------
+       PROGRAM-BEGIN.
+
+           DISPLAY 'STUDENT-ID: '
+           ACCEPT WS-STUDENT-ID.
+           DISPLAY 'NAME: '
+           ACCEPT WS-NAME.
+
+           OPEN OUTPUT STUDENT.
+               WRITE STUDENT-FILE FROM WS-STUDENT
+               END-WRITE.
+           CLOSE STUDENT.
+           GOBACK.
+
+       PROGRAM-DONE.
