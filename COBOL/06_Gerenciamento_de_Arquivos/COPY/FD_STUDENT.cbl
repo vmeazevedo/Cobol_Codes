1c5205b35cb55d4e16b63b1dae809788328551e5
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-16 17:06:44 -0300

    Create FD_STUDENT.cbl

---
+++ COBOL/06_Gerenciamento_de_Arquivos/COPY/FD_STUDENT.cbl
@@ -0,0 +1,5 @@
+       FD  STUDENT.
+       01  STUDENT-FILE.
+           05  STUDENT-ID           PIC 9(5).
+           05  FILLER               PIC X(1).
+           05  STUDENT-NAME         PIC X(25).
